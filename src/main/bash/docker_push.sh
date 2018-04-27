527b72e9a6abf267aa3be9a245da1a6c370617cd
Andre de Fontana Ignacio <andre.ignacio@netshoes.com>
2018-04-27 11:38:48 -0300

    Fix DOCKER_USERNAME and DOCKER_PASSWORD in .travis.yml Push latest docker tag

--- src/main/bash/docker_push.sh
+++ src/main/bash/docker_push.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push netshoes/sample-sleuth-users:latest
 docker push netshoes/sample-sleuth-users:1.0-SNAPSHOT
